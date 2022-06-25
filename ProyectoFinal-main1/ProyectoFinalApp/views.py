30282298010132638cc0702f77ef81a38ff42eee
Bernardo-Centeno <ber.centeno99@gmail.com>
2022-06-25 13:20:14 -0300

    vista, url de busqueda creados

---
+++ ProyectoFinal-main1/ProyectoFinalApp/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def inicio (request):
+    return render (request, "ProyectoFinalApp\main.html", {} )
+
+def cursos (request):
+    cursos = Curso.objects.all()
+    ctx={'cursos':cursos}
+    return render (request, "ProyectoFinalApp\cursos.html", ctx  )
+
+def eventos (request):
+    eventos = Evento.objects.all()
+    ctx1={'eventos':eventos}
+    return render (request, "ProyectoFinalApp\eventos.html", ctx1  )
+
+def agregar (request):
+    
+    
+#get
+    if request.method =="GET":
+        return render (request, r"ProyectoFinalApp\agregar.html",{})
+#post
+    elif request.method == "POST":
+        
+        print(request)
+        # info_formulario = request.POST
+        
+        # curso = Curso(info_formulario["nombre"], info_formulario["comision"])
+        # curso.save()
+        return render (request, "ProyectoFinalApp\main.html", {} )
+
+def agregar_pipeta (request):
+#post
+    if request.method == "POST":
+
+        info_formulario = request.POST
+        
+        pipeta = Pipeta(tipo = info_formulario["tipo"], nombre = info_formulario["nombre"], peso = int(info_formulario["peso"]), precio = int(info_formulario["precio"]))
+        pipeta.save()
+        return render (request, r"ProyectoFinalApp\agregar.html",{})
+    
+    else: #get y otros
+        return render (request, r"ProyectoFinalApp\agregar_pipeta.html",{})
+
+def agregar_collar (request):
+#post
+    if request.method == "POST":
+
+        info_formulario = request.POST
+        
+        collar = Collar(largo = int(info_formulario["largo"]), color= info_formulario["color"],precio = int(info_formulario["precio"]))
+        collar.save()
+        return render (request, r"ProyectoFinalApp\agregar.html",{})
+    
+    else: #get y otros
+        return render (request, r"ProyectoFinalApp\agregar_collar.html",{})
+
+def agregar_comida (request):
+#post
+    if request.method == "POST":
+
+        info_formulario = request.POST
+        
+        comida = Comida(tipo = info_formulario["tipo"], tamaño = info_formulario["tamaño"],nombre = info_formulario["nombre"],peso = int(info_formulario["peso"]),precio = int(info_formulario["precio"]))
+        comida.save()
+        return render (request, r"ProyectoFinalApp\agregar.html",{})
+    
+    else: #get y otros
+        return render (request, r"ProyectoFinalApp\agregar_comida.html",{})
+
+
+def buscar_comidas(request):
+    if request.method == "POST":
+               
+        comida = request.POST["producto"]
+        
+        comidas = Comida.objects.filter(tipo__icontains=comida, tamaño__icontains=comida, peso__icontains=comida, presio__icontains=comida, nombre__icontains=comida)
+        
+        return render(request,"ProyectoCoderApp/buscar_comidas.html",{"comidas":comidas})
+
+    else: # get y otros
+        comidas = [] # Curso.objects.all()
+    
+        #return render(request,"ProyectoCoderApp/busqueda_comision.html",{"comisiones":comisiones})
+        return render(request, "ProyectoFinalApp/buscar_comidas.html",{"comidas":comidas})
+
+
